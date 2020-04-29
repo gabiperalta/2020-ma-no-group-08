c771905f82b86b9fc449052c9152674bcd27841f
gaboberna <gabobernardini@gmail.com>
2020-04-29 00:03:44 -0300

    agrego expresion regular para validacion de contraseña

---
+++ src/main/java/ValidadorContraseña.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class ValidadorContraseña {
+    public static void main(String[] args) throws Exception {
+        ValidadorContraseña validador =  new ValidadorContraseña();
+        validador.esContraseniaValida("F@rd1coSports");
+
+    }
+
+    public boolean esContraseniaValida (String contrasenia) throws Exception {
+        return this.noEstaEntrePeoresContrasenias(contrasenia) & this.cumpleRequisitosRegEx(contrasenia);
+    }
+
+    private boolean noEstaEntrePeoresContrasenias(String contrasenia) throws Exception {
+        File file = new File("src/main/resources/worstPasswords");
+        Scanner sc = new Scanner(file);
+
+        while (sc.hasNextLine()){
+            if (sc.nextLine().equalsIgnoreCase(contrasenia)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean cumpleRequisitosRegEx(String contrasenia) {
+        String strRegEx = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*](?=\\S+$).{8,15}$";
+
+        return contrasenia.matches(strRegEx);
+    }
+
+
+} 
