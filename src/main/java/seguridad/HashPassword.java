8f94e1b7a43a913299a5f9a34198128465153d14
gabiperalta <gabiperalta16398@gmail.com>
2020-05-03 17:11:14 -0300

    Agrego clase para el hash del password

---
+++ src/main/java/seguridad/HashPassword.java
@@ -0,0 +1,17 @@
+package seguridad;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class HashPassword {
+    private static String algoritmo = "SHA-384";
+
+    public static String calcular(String password) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance(algoritmo);
+        digest.reset();
+        byte[] hash = digest.digest(password.getBytes());
+        return Base64.getEncoder().encodeToString(hash);
+    }
+
+}
