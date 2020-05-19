17b5d0095a5475a524edec60b5411e0d452d90bf
gaboberna <gabobernardini@gmail.com>
2020-05-18 21:04:02 -0300

    agrego test al validador de contrasenia

--- src/test/java/ValidadorTest.java
+++ src/test/java/ValidadorTest.java
@@ -14,12 +14,38 @@ public class ValidadorTest extends TestCase {
         target =  new ValidadorContrasenia();
 
         contrasenias = new ArrayList<String>();
-        contrasenias.add("Gaboberna1!");
-        contrasenias.add("Gaboberna2´");
-        contrasenias.add("Gaboberna3/");
+        contrasenias.add("Contrasenia1!");
+        contrasenias.add("Contrasenia2´");
+        contrasenias.add("Contrasenia3/");
     }
 
     public void testContraseniaValida() throws Exception {
-        assertTrue(target.esContraseniaValida("Gaboberna4*", contrasenias));
+        assertTrue(target.esContraseniaValida("Contrasenia4*", contrasenias));
     }
+
+    public void testContraseniaInvalidaPorSerDeLasPeores() throws Exception {
+        assertFalse(target.esContraseniaValida("danielle", contrasenias));
+    }
+
+    public void testContraseniaInvalidaPorNoRespetarExpresionRegularPorCaracterEspecial() throws Exception {
+        assertFalse(target.esContraseniaValida("Contrasenia1", contrasenias));
+    }
+
+    public void testContraseniaInvalidaPorNoRespetarExpresionRegularPorNumeros() throws Exception {
+        assertFalse(target.esContraseniaValida("Contrasenia+", contrasenias));
+    }
+
+    public void testContraseniaInvalidaPorNoRespetarExpresionRegularPorMayusculas() throws Exception {
+        assertFalse(target.esContraseniaValida("contrasenia1+", contrasenias));
+    }
+
+    public void testContraseniaInvalidaPorNoRespetarExpresionRegularPorEspacio() throws Exception {
+        assertFalse(target.esContraseniaValida("Contrasenia1+ ", contrasenias));
+    }
+
+    public void testContraseniaInvalidaPorSerRepetida() throws Exception {
+        assertFalse(target.esContraseniaValida("Contrasenia3/", contrasenias));
+    }
+
+
 }
