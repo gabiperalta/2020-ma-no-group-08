9d4e64d8c2327e983cb776bd09ea24afbbd75834
Giuliana <giuliposteraro@hotmail.com>
2020-06-07 21:19:43 -0300

    Empezando con la parte de presupuestos

---
+++ src/main/java/dominio/presupuestos/Presupuesto.java
@@ -0,0 +1,11 @@
+package dominio.presupuestos;
+
+import java.util.ArrayList;
+
+import dominio.operaciones.EntidadOperacion;
+import dominio.operaciones.Item;
+
+public class Presupuesto {
+	private EntidadOperacion proveedor;
+	private ArrayList<Item> items;
+}
