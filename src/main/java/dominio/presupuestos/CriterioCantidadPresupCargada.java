8a8b24a724e58d01cef9804d21a4bb2689b579ac
fedebanca <fedebanca97@gmail.com>
2020-06-09 20:49:13 -0300

    El validador manda el mensaje al notificador

--- src/main/java/dominio/presupuestos/CriterioCantidadPresupCargada.java
+++ src/main/java/dominio/presupuestos/CriterioCantidadPresupCargada.java
@@ -1,22 +1,37 @@
-package dominio.operaciones;
+package dominio.presupuestos;
 
 import java.util.ArrayList;
 
 import dominio.excepciones.LaCompraNoRequierePresupuestosException;
-import dominio.presupuestos.CriterioLicitacion;
-import dominio.presupuestos.Presupuesto;
+import dominio.operaciones.OperacionEgreso;
 
 public class CriterioCantidadPresupCargada implements CriterioLicitacion{
+	
+	boolean resultado;
 
 	@Override
 	public boolean validar(OperacionEgreso operacion, ArrayList<Presupuesto> presupuestos) {
 		if(operacion.getPresupuestosNecesarios() != 0) {      //Verifico que la compra requiera presupuestos 
-			return operacion.getPresupuestosNecesarios() !=0; //Verifico que este cargada la cantidad de presupuestos
+			resultado = operacion.getPresupuestosNecesarios() !=0; //Verifico que este cargada la cantidad de presupuestos
+			return resultado;
 		}
 		else {
 			throw new LaCompraNoRequierePresupuestosException("La compra no requiere presupuestos");
 		}
 			
+	}
+	
+	@Override
+	public String descripcion() {
+		String descripcion;
+		if(resultado) {
+			descripcion = "Criterio de cantidad de presupuestos: Valido";
+		}
+		else {
+			descripcion = "Criterio de cantidad de presupuestos: Invalido";
+		}
+		
+		return descripcion;
 	}
 	
 }
