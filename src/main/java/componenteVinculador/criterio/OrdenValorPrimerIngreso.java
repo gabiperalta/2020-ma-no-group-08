1a5441e52be6749289fa9c86c1519544b1a48c25
gaboberna <gabobernardini@gmail.com>
2020-08-13 01:29:56 -0300

    agrego test del OrdenValorPrimerIngreso

--- src/main/java/componenteVinculador/criterio/OrdenValorPrimerIngreso.java
+++ src/main/java/componenteVinculador/criterio/OrdenValorPrimerIngreso.java
@@ -6,9 +6,40 @@ import java.util.ArrayList;
 
 public class OrdenValorPrimerIngreso implements CriterioVinculacion {
 
+    private ArrayList<ResultadoVinculado> resultados;
+
     @Override
     public void ejecutar(ArrayList<OperacionVinculable> ingresos, ArrayList<OperacionVinculable> egresos) {
         this.ordenar(ingresos,egresos);
+        resultados = new ArrayList<>();
+
+        for (OperacionVinculable ingreso : ingresos) {
+            for (OperacionVinculable egreso : egresos) {
+                ResultadoVinculado resultado = this.buscaOCreaResultadoNuevo(ingreso);
+
+                if(resultado.sePuedeVincularEgreso(egreso)){
+                    resultado.vincularNuevoEgreso(egreso);
+
+                    agregarResultadoSiEsNecesario(resultado);
+                    break;
+                }
+            }
+        }
+    }
+
+    private void agregarResultadoSiEsNecesario(ResultadoVinculado resultadoVinculado) {
+        if(resultadoVinculado.getEgresos().size() == 1){
+            resultados.add(resultadoVinculado);
+        }
+    }
+
+    private ResultadoVinculado buscaOCreaResultadoNuevo(OperacionVinculable ingreso) {
+        for (ResultadoVinculado resultado:resultados) {
+            if (resultado.contieneAlIngreso(ingreso)) {
+                return resultado;
+            }
+        }
+        return new ResultadoVinculado(ingreso);
     }
 
     @Override
@@ -20,7 +51,7 @@ public class OrdenValorPrimerIngreso implements CriterioVinculacion {
 
     @Override
     public ArrayList<ResultadoVinculado> getResultadosVinculados() {
-
-        return null;
+        return resultados;
     }
+
 }
---
+++ src/test/java/componenteVinculador/criterio/OrdenValorPrimerIngresoTest.java
@@ -0,0 +1,90 @@
+package componenteVinculador.criterio;
+
+import componenteVinculador.criterio.ResultadoVinculado.ResultadoVinculado;
+import componenteVinculador.vinculable.ETipoOperacionVinculable;
+import componenteVinculador.vinculable.OperacionVinculable;
+import componenteVinculador.vinculable.utils.FechaUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class OrdenValorPrimerIngresoTest {
+    private OrdenValorPrimerEgreso target;
+    private OperacionVinculable ingreso1;
+    private OperacionVinculable ingreso2;
+    private OperacionVinculable egreso1;
+    private OperacionVinculable egreso2;
+    private OperacionVinculable egreso3;
+    private OperacionVinculable egreso4;
+    private ArrayList<OperacionVinculable> ingresos;
+    private ArrayList<OperacionVinculable> egresos;
+
+    @Before
+    public void setUp() {
+        target = new OrdenValorPrimerEgreso();
+        ingresos = new ArrayList<>();
+        egresos = new ArrayList<>();
+
+        ingreso1 = new OperacionVinculable(1000,new Date(), ETipoOperacionVinculable.INGRESO);
+        ingreso2 = new OperacionVinculable(2000,new Date(), ETipoOperacionVinculable.INGRESO);
+
+        egreso1 = new OperacionVinculable(500, FechaUtils.obtenerFechaDiasAtras(1), ETipoOperacionVinculable.EGRESO);
+        egreso2 = new OperacionVinculable(300, FechaUtils.obtenerFechaDiasAtras(5), ETipoOperacionVinculable.EGRESO);
+        egreso3 = new OperacionVinculable(350, FechaUtils.obtenerFechaDiasAtras(3), ETipoOperacionVinculable.EGRESO);
+        egreso4 = new OperacionVinculable(2100,new Date(), ETipoOperacionVinculable.EGRESO);
+    }
+    @Test
+    public void vinculacionConDosResultadosOk() {
+        ingresos.add(ingreso1);
+        ingresos.add(ingreso2);
+
+        egresos.add(egreso1);
+        egresos.add(egreso2);
+        egresos.add(egreso3);
+
+        target.ejecutar(ingresos,egresos);
+
+        ResultadoVinculado resultado1 = new ResultadoVinculado(ingreso1);
+        resultado1.vincularNuevoEgreso(egreso2);
+        resultado1.vincularNuevoEgreso(egreso3);
+
+        ResultadoVinculado resultado2 = new ResultadoVinculado(ingreso2);
+        resultado2.vincularNuevoEgreso(egreso1);
+
+        assertTrue(resultado1.equals(target.getResultadosVinculados().get(0)) && resultado2.equals(target.getResultadosVinculados().get(1)));
+    }
+
+    @Test
+    public void vinculacionConEgresoSinVincularPorMontoOk() {
+        ingresos.add(ingreso1);
+        ingresos.add(ingreso2);
+
+        egresos.add(egreso4);
+
+        target.ejecutar(ingresos,egresos);
+        assertEquals(target.getResultadosVinculados().size(),0);
+    }
+
+    @Test
+    public void vinculacionSinIngresos() {
+        egresos.add(egreso1);
+        egresos.add(egreso2);
+        egresos.add(egreso3);
+
+        target.ejecutar(ingresos,egresos);
+        assertEquals(target.getResultadosVinculados().size(),0);
+    }
+
+    @Test
+    public void vinculacionSinEgresos() {
+        ingresos.add(ingreso1);
+        ingresos.add(ingreso2);
+
+        target.ejecutar(ingresos,egresos);
+        assertEquals(target.getResultadosVinculados().size(),0);
+    }
+}
