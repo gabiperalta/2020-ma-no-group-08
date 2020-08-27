feb1c756dc03d48637df9d89dbb43ae53bdcc4d5
gabrielbernardini <gbernardini@gmail.com>
2020-08-27 01:51:41 -0300

    vinculador como singleton

--- src/main/java/componenteVinculador/vinculador/Vinculador.java
+++ src/main/java/componenteVinculador/vinculador/Vinculador.java
@@ -9,6 +9,20 @@ import java.util.ArrayList;
 
 public class Vinculador {
     private ArrayList<CriterioVinculacion> criterios;
+    private static Vinculador shared;
+
+    public  static Vinculador getInstance() {
+
+        if (shared == null) {
+
+            shared = new Vinculador();
+        }
+        return shared;
+    }
+
+    private Vinculador(){
+        criterios = new ArrayList<>();
+    }
 
     public void vincular(ArrayList<OperacionVinculable> unosIngresos, ArrayList<OperacionVinculable> unosEgresos,
                          ArrayList<CriterioVinculacion> unosCriterios, int rangoDias) {
