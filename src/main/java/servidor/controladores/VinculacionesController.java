453a11568735406762d9e178690b2a50df1434a3
gaboberna <gabobernardini@gmail.com>
2020-10-09 12:08:01 -0300

    generar criterio para vincular

--- src/main/java/servidor/controladores/VinculacionesController.java
+++ src/main/java/servidor/controladores/VinculacionesController.java
@@ -1,5 +1,7 @@
 package servidor.controladores;
 
+import componenteVinculador.criterio.GeneradorCriterio;
+import componenteVinculador.criterio.vinculacion.CriterioVinculacion;
 import dominio.cuentasUsuarios.CuentaUsuario;
 import dominio.operaciones.OperacionEgreso;
 import dominio.operaciones.OperacionIngreso;
@@ -32,11 +34,13 @@ public class VinculacionesController {
         return new ModelAndView(parameters, "vinculaciones.hbs");
     }
 
-    public Response vincular(Request request, Response response) {
+    public Response vincular(Request request, Response response) throws Exception {
         Map<String, Object> parameters = new HashMap<>();
         Set<String> queryP = request.queryParams();
-
+        String tipoCriterio = request.queryParams("criterio");
+        Integer rangoDias = Integer.valueOf(request.queryParams("rangoDias"));
 //        TODO: falta descargar el json
+        CriterioVinculacion criterio = GeneradorCriterio.generarCriterio(tipoCriterio,rangoDias);
         response.redirect("/home", 302);
         return response;
     }
