82bec36f3f4be168a117a973003656dc7c0923ad
carolinacampos <carolina.campos@mercadolibre.com>
2020-10-21 18:50:12 -0300

    cambio medio pago

--- src/main/java/servicio/serviceMercadoLibre.java
+++ src/main/java/servicio/serviceMercadoLibre.java
@@ -16,55 +16,16 @@ public enum serviceMercadoLibre {
     private RestClientML restClient = RestClientML.INSTANCE;
 
 
-    public JSONArray getCountries() throws Exception {
-        String url = "https://api.mercadolibre.com/classified_locations/countries";
-        return JsonUtils.getResponseJsonArray(restClient.get(url, new Headers()));
-    }
-
-    public JSONObject getCountry(String country) throws Exception {
-        String url = "https://api.mercadolibre.com/classified_locations/countries/" + country;
-        return JsonUtils.getLinkedHashMapToJson(restClient.get(url, new Headers()));
-    }
-
-    public JSONObject getState(String state) throws Exception {
-        String url = "https://api.mercadolibre.com/classified_locations/states/" + state;
-        return JsonUtils.getLinkedHashMapToJson(restClient.get(url, new Headers()));
-    }
-
-    public JSONObject getCity(String city) throws Exception {
-        String url = "https://api.mercadolibre.com/classified_locations/cities/" + city;
-        return JsonUtils.getLinkedHashMapToJson(restClient.get(url, new Headers()));
-    }
-
-    public JSONArray getMonedas() throws Exception {
-        String url = "https://api.mercadolibre.com/currencies/";
-        return JsonUtils.getResponseJsonArray(restClient.get(url, new Headers()));
-    }
-
-
-    public JSONObject getMoneda(String moneda) throws Exception {
-        String url = "https://api.mercadolibre.com/currencies/" + moneda;
-        return JsonUtils.getLinkedHashMapToJson(restClient.get(url, new Headers()));
-    }
-
-    public JSONObject getConversion(String monedaInicio, String monedaFin) throws Exception {
-        String url = "https://api.mercadolibre.com/currency_conversions/search?from="+monedaInicio+"&to="+monedaFin;
-        return JsonUtils.getLinkedHashMapToJson(restClient.get(url, new Headers()));
-    }
-
-    public JSONObject getCountryWithZipCode(String country, String zipCode) throws Exception {
-        String url = "https://api.mercadolibre.com/countries/"+country+"/zip_codes/"+zipCode;
-        return JsonUtils.getLinkedHashMapToJson(restClient.get(url, new Headers()));
-    }
-
-    public JSONObject getCountryWithZipCode(String country, String zipCodeInit, String zipCodeFinal) throws Exception {
-        String url = "https://api.mercadolibre.com/country/"+country+"/zip_codes/search_between?zip_code_from="+zipCodeInit+"&zip_code_to="+zipCodeFinal;
-        return JsonUtils.getLinkedHashMapToJson(restClient.get(url, new Headers()));
-    }
-
     public JSONObject getMediosDePago() throws Exception {
         String url = "https://api.mercadopago.com/v1/payment_methods";
         return JsonUtils.getLinkedHashMapToJson(restClient.get(url, headersMeli()));
+    }
+
+    public Object getMediosDePago() throws Exception {
+        String url = "https://api.mercadopago.com/v1/payment_methods";
+
+        Gson gson = new Gson();
+        return gson.fromJson(JsonUtils.getResponseJsonArray(restClient.get(url, new Headers())).toString(), Currency[].class);
     }
 
     public static Headers headersMeli() throws Exception {
