19f2925e5113a3f7e6b40bd2cbc2171e49b668e6
gaboberna <gabobernardini@gmail.com>
2020-10-27 02:43:32 -0300

    borro codigo del repo de organizacion

--- src/main/java/datos/RepoOrganizaciones.java
+++ src/main/java/datos/RepoOrganizaciones.java
@@ -8,31 +8,10 @@ import javax.persistence.EntityManager;
 import java.util.ArrayList;
 
 public class RepoOrganizaciones {
-    //private static ArrayList<Organizacion> organizaciones;
     private EntityManager entityManager;
 
-//    private static class RepositorioOrganizacionesHolder {
-//        static final RepoOrganizaciones singleInstanceRepositorioOrganizaciones = new RepoOrganizaciones();
-//    }
-//
-//    public static RepoOrganizaciones getInstance() {
-//        return RepoOrganizaciones.RepositorioOrganizacionesHolder.singleInstanceRepositorioOrganizaciones;
-//    }
-
-    //public RepoOrganizaciones() {} //TODO: eliminar
     public RepoOrganizaciones(EntityManager em) {
         entityManager = em;
-
-//TODO: revisar
-//        Organizacion organizacion1 = new Organizacion("org1", null);
-//        Organizacion organizacion2 = new Organizacion("org2",null);
-//        Organizacion organizacion3 = new Organizacion("org3",null);
-//
-//        organizaciones = new ArrayList<Organizacion>();
-//
-//        organizaciones.add(organizacion1);
-//        organizaciones.add(organizacion2);
-//        organizaciones.add(organizacion3);
     }
 
     public void agregarOrganizacion(String nombreOrganizacion, ArrayList<Empresa> entidades) {
